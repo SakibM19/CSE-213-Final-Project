f28c3c29a5238257e7cd5d1e92b3adb42f24d45e
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-08 21:49:15 +0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cse213finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cse213finalproject to javafx.fxml;
+    exports com.example.cse213finalproject;
+}
