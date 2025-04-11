25e5d73e5d31a7e152b0858c64798383b1c56a9b
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-11 16:26:30 +0600

    Added payment selection fxml

---
+++ src/main/java/sakib/CustomerPaymentMethodSelectViewController.java
@@ -0,0 +1,14 @@
+package sakib;
+
+import javafx.event.Event;
+
+public class CustomerPaymentMethodSelectViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+}
