bfd58b07a5f7b23ea60a25c2583af5fe61af467e
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-11 10:36:57 +0600

    Modified customer Dashboard

--- src/main/java/com/example/cse213finalproject/CustomerDashboardViewController.java
+++ src/main/java/com/example/cse213finalproject/CustomerDashboardViewController.java
@@ -3,5 +3,41 @@ package com.example.cse213finalproject;
 public class CustomerDashboardViewController
 {
     @javafx.fxml.FXML
+    private TextField sendMessageToCustomerServiceTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void couponsAndVouchersOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void myFavouritesOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void emergencyAssistanceOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageLocationOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void tracKBookingOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void placeOrderOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void paymentOnMouseClickedButton(Event event) {
+    }
+}
