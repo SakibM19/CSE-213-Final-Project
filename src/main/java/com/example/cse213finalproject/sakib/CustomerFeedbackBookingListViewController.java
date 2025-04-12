ec8dbabc0ceebfbbfe5fa79094c07c3822cb4e20
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-12 17:02:46 +0600

    added customer feedback fxml

---
+++ src/main/java/com/example/cse213finalproject/sakib/CustomerFeedbackBookingListViewController.java
@@ -0,0 +1,47 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFeedbackBookingListViewController
+{
+    @javafx.fxml.FXML
+    private TextField additionalCommentsTextField;
+    @javafx.fxml.FXML
+    private TableColumn additionalCommentTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn bookingIdTableColumn;
+    @javafx.fxml.FXML
+    private TableView feedbackTableView;
+    @javafx.fxml.FXML
+    private ComboBox comfortComboBox;
+    @javafx.fxml.FXML
+    private TableColumn cleanlinessTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn vehicleNumberTableColumn;
+    @javafx.fxml.FXML
+    private TextField bookingIdInputTextField;
+    @javafx.fxml.FXML
+    private TableColumn comfortTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn vehicleConditionTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox vehicleConditionComboBox;
+    @javafx.fxml.FXML
+    private ComboBox cleanlinessComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitFeedbackOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+}
