9c3c46b04309690e5193d30b2a72df1d2f2f2485
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-12 10:05:07 +0600

    Added booking Status FXML

---
+++ src/main/java/com/example/cse213finalproject/sakib/CustomerCurrentBookingStatusViewController.java
@@ -0,0 +1,36 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.Label;
+
+public class CustomerCurrentBookingStatusViewController
+{
+    @javafx.fxml.FXML
+    private Label currentStatusLabel;
+    @javafx.fxml.FXML
+    private Label vehicleTypeLabel;
+    @javafx.fxml.FXML
+    private Label dropoffTimeLabel;
+    @javafx.fxml.FXML
+    private Label dropoffDateLabel;
+    @javafx.fxml.FXML
+    private Label pickupDateLabel;
+    @javafx.fxml.FXML
+    private Label totalCostLabel;
+    @javafx.fxml.FXML
+    private Label bookingIdLabel;
+    @javafx.fxml.FXML
+    private Label pickupTimeLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelBookingOnMouseClickedButton(Event event) {
+    }
+}
---
+++ src/main/java/com/example/cse213finalproject/sakib/CustomerTrackBookingViewController.java
@@ -0,0 +1,28 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerTrackBookingViewController
+{
+    @javafx.fxml.FXML
+    private TableView trackBookingTableView;
+    @javafx.fxml.FXML
+    private TableColumn activeBookingIdTableColumn;
+    @javafx.fxml.FXML
+    private TextField idForTrackingTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackBookingOnMouseClickedButton(Event event) {
+    }
+}
