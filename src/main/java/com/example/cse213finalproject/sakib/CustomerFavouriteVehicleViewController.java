6bba94212e7915df1fd76dda63b03793443a30bc
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-12 21:45:34 +0600

    Added Customer Favourite Vehicle Fxml

---
+++ src/main/java/com/example/cse213finalproject/sakib/CustomerFavouriteVehicleViewController.java
@@ -0,0 +1,50 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFavouriteVehicleViewController
+{
+    @javafx.fxml.FXML
+    private TableColumn totalCostTableColumn;
+    @javafx.fxml.FXML
+    private TextField enterVehicleIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn pastOrderIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn vehicleModelTableColumn;
+    @javafx.fxml.FXML
+    private TextField enterOrderIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn vehicleIdFavouriteTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn overallFeedbackOnVehicleTableColumn;
+    @javafx.fxml.FXML
+    private TableView favouriteVehicleTableView;
+    @javafx.fxml.FXML
+    private TableColumn vehicleModelFavouriteTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn pickupDateTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn vehicleIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn vehicleTypeTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn lastOrderIdTableColumn;
+    @javafx.fxml.FXML
+    private TableView pastOrderVehicleListTableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void addToFavouriteOnMouseClickedButton(Event event) {
+    }
+}
