58983b75d1c3b933211e688fa587b65c66fb5b08
alveesarker <asahanaz305@gmail.com>
2025-04-13 11:37:36 +0600

    completed complaint fxml

---
+++ src/main/java/com/example/cse213finalproject/alvee/controller/ComplaintDetailController.java
@@ -0,0 +1,25 @@
+package com.example.cse213finalproject.alvee.controller;
+
+public class ComplaintDetailController
+{
+    @javafx.fxml.FXML
+    private Text dateText;
+    @javafx.fxml.FXML
+    private Text carNameText;
+    @javafx.fxml.FXML
+    private Label complaintDesLabel;
+    @javafx.fxml.FXML
+    private Text customrEmailText;
+    @javafx.fxml.FXML
+    private Text bookingIdText;
+    @javafx.fxml.FXML
+    private Text customerNameText;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleResovedButtonOnAction(ActionEvent actionEvent) {
+    }
+}
