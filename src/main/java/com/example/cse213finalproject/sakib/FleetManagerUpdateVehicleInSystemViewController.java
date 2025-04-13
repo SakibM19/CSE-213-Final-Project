03d988b38ce2f156d09495ffbddc9ec9d9c34e50
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-13 18:33:33 +0600

    added fleetmanager manage vehicle fxml

---
+++ src/main/java/com/example/cse213finalproject/sakib/FleetManagerUpdateVehicleInSystemViewController.java
@@ -0,0 +1,26 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+
+public class FleetManagerUpdateVehicleInSystemViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addVehicleOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeVehicleOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateVehicleOnMouseClickedButton(Event event) {
+    }
+}
