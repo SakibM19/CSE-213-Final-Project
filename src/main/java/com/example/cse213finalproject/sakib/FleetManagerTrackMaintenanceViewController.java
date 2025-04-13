4bf26e503dfc83e808b00825206900fa82cbc0e1
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-13 21:27:24 +0600

    fleetManager track maintenance Fxml

---
+++ src/main/java/com/example/cse213finalproject/sakib/FleetManagerTrackMaintenanceViewController.java
@@ -0,0 +1,34 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.Label;
+
+public class FleetManagerTrackMaintenanceViewController
+{
+    @javafx.fxml.FXML
+    private Label vehicleTypeLabel;
+    @javafx.fxml.FXML
+    private Label nextMaintananceDateLabel;
+    @javafx.fxml.FXML
+    private Label vehicleIdLabel;
+    @javafx.fxml.FXML
+    private Label currentMaintananceStatusLabel;
+    @javafx.fxml.FXML
+    private Label lastMaintananceDateLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void getVehicleStatusOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateVehicleStatusOnMouseClickedButton(Event event) {
+    }
+}
