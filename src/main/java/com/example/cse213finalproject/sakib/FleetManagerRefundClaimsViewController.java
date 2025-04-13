2d68bd057efb94a622fa803f1e9e717f2df134e7
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-14 02:03:25 +0600

    Added the maintenance crew fxml

--- src/main/java/com/example/cse213finalproject/sakib/FleetManagerRefundClaimsViewController.java
+++ src/main/java/com/example/cse213finalproject/sakib/FleetManagerRefundClaimsViewController.java
@@ -1,10 +1,11 @@
 package com.example.cse213finalproject.sakib;
 
+import javafx.event.Event;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import jdk.jfr.Event;
+
 
 public class FleetManagerRefundClaimsViewController
 {
---
+++ src/main/java/com/example/cse213finalproject/sakib/FleetManagerScheduleMaintenanceViewController.java
@@ -0,0 +1,46 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+import javafx.scene.control.*;
+
+public class FleetManagerScheduleMaintenanceViewController
+{
+    @javafx.fxml.FXML
+    private CheckBox routineCheckupCheckBox;
+    @javafx.fxml.FXML
+    private TableView blockedVehicleListTableView;
+    @javafx.fxml.FXML
+    private CheckBox cleaningCheckBox;
+    @javafx.fxml.FXML
+    private TableColumn maintenanceTypeTableView;
+    @javafx.fxml.FXML
+    private TableColumn blockedVehicleIdTableView;
+    @javafx.fxml.FXML
+    private CheckBox oilChangeCheckBox;
+    @javafx.fxml.FXML
+    private CheckBox tireReplacementCheckBox;
+    @javafx.fxml.FXML
+    private TextField enterVehicleIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox selectCrewComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void assignOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkOnVehicleAndCrewOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshOnMouseClickedButton(Event event) {
+    }
+}
