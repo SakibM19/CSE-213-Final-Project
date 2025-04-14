d1ab179d077f11d2d8e93a7ee2344c20e59f2f98
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-15 00:38:27 +0600

    worked on multiple fxml

--- src/main/java/com/example/cse213finalproject/sakibModelClass/Payment.java
+++ src/main/java/com/example/cse213finalproject/sakibModelClass/Payment.java
@@ -1,15 +1,16 @@
 package com.example.cse213finalproject.sakibModelClass;
 
 public class Payment {
-    private int paymentAmount;
+    private int paymentAmount, voucherAmount;
     private String paymentType;
 
 
     public Payment() {
     }
 
-    public Payment(int paymentAmount, String paymentType) {
+    public Payment(int paymentAmount, int refundAmount, String paymentType) {
         this.paymentAmount = paymentAmount;
+        this.voucherAmount = refundAmount;
         this.paymentType = paymentType;
     }
 
@@ -29,10 +30,19 @@ public class Payment {
         this.paymentType = paymentType;
     }
 
+    public int getRefundAmount() {
+        return voucherAmount;
+    }
+
+    public void setRefundAmount(int refundAmount) {
+        this.voucherAmount = refundAmount;
+    }
+
     @Override
     public String toString() {
         return "Payment{" +
                 "paymentAmount=" + paymentAmount +
+                ", refundAmount=" + voucherAmount +
                 ", paymentType='" + paymentType + '\'' +
                 '}';
     }
--- src/main/java/com/example/cse213finalproject/sakibModelClass/Voucher.java
+++ src/main/java/com/example/cse213finalproject/sakibModelClass/Voucher.java
@@ -1,4 +1,83 @@
 package com.example.cse213finalproject.sakibModelClass;
 
+import java.time.LocalDate;
+
 public class Voucher {
+    private String voucherCode, offerName, offerDetail;
+    private int offerAmount;
+    private float offerPercentage;
+    private LocalDate expiryDate;
+
+
+    public Voucher() {
+    }
+
+    public Voucher(String voucherCode, String offerName, String offerDetail, int offerAmount, float offerPercentage, LocalDate expiryDate) {
+        this.voucherCode = voucherCode;
+        this.offerName = offerName;
+        this.offerDetail = offerDetail;
+        this.offerAmount = offerAmount;
+        this.offerPercentage = offerPercentage;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getVoucherCode() {
+        return voucherCode;
+    }
+
+    public void setVoucherCode(String voucherCode) {
+        this.voucherCode = voucherCode;
+    }
+
+    public String getOfferName() {
+        return offerName;
+    }
+
+    public void setOfferName(String offerName) {
+        this.offerName = offerName;
+    }
+
+    public String getOfferDetail() {
+        return offerDetail;
+    }
+
+    public void setOfferDetail(String offerDetail) {
+        this.offerDetail = offerDetail;
+    }
+
+    public int getOfferAmount() {
+        return offerAmount;
+    }
+
+    public void setOfferAmount(int offerAmount) {
+        this.offerAmount = offerAmount;
+    }
+
+    public float getOfferPercentage() {
+        return offerPercentage;
+    }
+
+    public void setOfferPercentage(float offerPercentage) {
+        this.offerPercentage = offerPercentage;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Voucher{" +
+                "voucherCode='" + voucherCode + '\'' +
+                ", offerName='" + offerName + '\'' +
+                ", offerDetail='" + offerDetail + '\'' +
+                ", offerAmount=" + offerAmount +
+                ", offerPercentage=" + offerPercentage +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
 }
