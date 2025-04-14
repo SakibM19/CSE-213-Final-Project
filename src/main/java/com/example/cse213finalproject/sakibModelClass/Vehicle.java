d0a4fe20dba37d0f4d226e2e00050ce7745291bb
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-14 16:32:50 +0600

    worked on vehicle

--- src/main/java/com/example/cse213finalproject/sakibModelClass/Vehicle.java
+++ src/main/java/com/example/cse213finalproject/sakibModelClass/Vehicle.java
@@ -1,4 +1,79 @@
 package com.example.cse213finalproject.sakibModelClass;
 
 public class Vehicle {
+    protected String vehicleID, vehicleModel, vehicleType, brand;
+    protected int seatCapacity;
+    protected float perDayCost;
+
+    public Vehicle(String vehicleID, String vehicleModel, String vehicleType, String brand, int seatCapacity, float perDayCost) {
+        this.vehicleID = vehicleID;
+        this.vehicleModel = vehicleModel;
+        this.vehicleType = vehicleType;
+        this.brand = brand;
+        this.seatCapacity = seatCapacity;
+        this.perDayCost = perDayCost;
+    }
+
+    public Vehicle() {
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getSeatCapacity() {
+        return seatCapacity;
+    }
+
+    public void setSeatCapacity(int seatCapacity) {
+        this.seatCapacity = seatCapacity;
+    }
+
+    public float getPerDayCost() {
+        return perDayCost;
+    }
+
+    public void setPerDayCost(float perDayCost) {
+        this.perDayCost = perDayCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleID='" + vehicleID + '\'' +
+                ", vehicleModel='" + vehicleModel + '\'' +
+                ", vehicleType='" + vehicleType + '\'' +
+                ", brand='" + brand + '\'' +
+                ", seatCapacity=" + seatCapacity +
+                ", perDayCost=" + perDayCost +
+                '}';
+    }
 }
