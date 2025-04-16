2e352db60d1c9ee59b2fc473b80010ac271761f7
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-16 13:50:19 +0600

    Fleetmanager class work

--- src/main/java/com/example/cse213finalproject/commonClass/Employee.java
+++ src/main/java/com/example/cse213finalproject/commonClass/Employee.java
@@ -1,12 +1,11 @@
 package com.example.cse213finalproject.commonClass;
 
 public abstract class  Employee extends User {
-    protected String employeeID, role;
+    protected String employeeID;
 
     public Employee(String name, String email, String password, int id, int phoneNumber, String employeeID, String role) {
         super(name, email, password, id, phoneNumber);
         this.employeeID = employeeID;
-        this.role = role;
     }
 
     public Employee() {
@@ -16,9 +15,8 @@ public abstract class  Employee extends User {
         super(name, email, password, id, phoneNumber);
     }
 
-    public Employee(String employeeID, String role) {
+    public Employee(String employeeID) {
         this.employeeID = employeeID;
-        this.role = role;
     }
 
     public String getEmployeeID() {
@@ -29,19 +27,11 @@ public abstract class  Employee extends User {
         this.employeeID = employeeID;
     }
 
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "employeeID='" + employeeID + '\'' +
-                ", role='" + role + '\'' +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
