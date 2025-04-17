d08de9271f8ac31b01e2b5f6ed11e62c897e723c
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-17 12:34:40 +0600

    fleetmanager Dashboard fixed

--- src/main/java/com/example/cse213finalproject/sakib/FleetManagerDashboardViewController.java
+++ src/main/java/com/example/cse213finalproject/sakib/FleetManagerDashboardViewController.java
@@ -1,7 +1,47 @@
 package com.example.cse213finalproject.sakib;
 
+import javafx.event.Event;
+
 public class FleetManagerDashboardViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+
+    @javafx.fxml.FXML
+    public void logOutOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackVehicleOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageFeedbackOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageRefundOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void blockVehicleOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void handelViolationOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageVehicleOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackMaintenanceOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleMaintenanceOnMouseClickedButton(Event event) {
+    }
+}
