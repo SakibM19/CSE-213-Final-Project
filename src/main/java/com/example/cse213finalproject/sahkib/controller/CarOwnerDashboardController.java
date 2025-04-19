ece99bc54a0d5c305745ec971fb028e5975def75
Sahkib159 <sahkib159@gmail.com>
2025-04-19 17:54:40 +0600

    added scene switcher

--- src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerDashboardController.java
+++ src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerDashboardController.java
@@ -1,6 +1,15 @@
 package com.example.cse213finalproject.sahkib.controller;
 
 import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import com.example.cse213finalproject.util.SceneSwitcher;
 
 public class CarOwnerDashboardController
 {
@@ -14,6 +23,7 @@ public class CarOwnerDashboardController
     public void rentListAction(Event event) {
     }
     public void addCarAction(Event event) {
+        switchScene("/com/example/cse213finalproject/sahkib/CarOwnerAddCarView.fxml", event);
     }
 
     @javafx.fxml.FXML
@@ -38,5 +48,18 @@ public class CarOwnerDashboardController
 
     @javafx.fxml.FXML
     public void maintainenceHistoryAction(Event event) {
+    }
+
+    private void switchScene(String fxmlFile, Event event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
