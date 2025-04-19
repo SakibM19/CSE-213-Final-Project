e0e90a8ed19d917f4c277938819a9f5cddbcae28
Sahkib159 <sahkib159@gmail.com>
2025-04-19 17:55:06 +0600

    removed unused import

--- src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerDashboardController.java
+++ src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerDashboardController.java
@@ -6,10 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
-import com.example.cse213finalproject.util.SceneSwitcher;
 
 public class CarOwnerDashboardController
 {
