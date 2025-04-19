9bde78aca876c7951a6dd3b025c07e4bbde58450
Sahkib159 <sahkib159@gmail.com>
2025-04-19 17:53:28 +0600

    added car add

---
+++ src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerAddCarController.java
@@ -0,0 +1,34 @@
+package com.example.cse213finalproject.sahkib.controller;
+
+import java.io.IOException;
+
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CarOwnerAddCarController {
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void backAction(Event event) {
+        switchScene("/com/example/cse213finalproject/sahkib/CarOwnerDashboardView.fxml", event);
+    }
+
+    private void switchScene(String fxmlFile, Event event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
