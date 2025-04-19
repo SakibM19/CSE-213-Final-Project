ce703a4ae4c2d64fc269c3e7c2491e2710097270
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-19 18:30:35 +0600

    location edit done

--- src/main/java/com/example/cse213finalproject/MainApplication.java
+++ src/main/java/com/example/cse213finalproject/MainApplication.java
@@ -10,11 +10,8 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/example/cse213finalproject/sakib/customerEmergencyAssistanceView.fxml"));
-=======
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/example/cse213finalproject/sakib/logInView.fxml"));
->>>>>>> 4c8e9620474424a584e2476028ab900a178e95e9
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Rent A Car");
         stage.setScene(scene);
