63814c149036775165c0eeee46e25b1cc84b0a0c
Sakib Mahmood <sakib.sv123@gmail.com>
2025-04-20 18:05:10 +0600

    P

---
+++ src/main/java/com/example/cse213finalproject/sakib/CustomerVouchersView.java
@@ -0,0 +1,18 @@
+package com.example.cse213finalproject.sakib;
+
+import javafx.event.Event;
+
+public class CustomerVouchersView
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnMouseClickedButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void voucherOnMouseClickedButton(Event event) {
+    }
+}
