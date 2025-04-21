45c0e7cc5b3f703c308932d2ebf6cbd98d2d919c
Sahkib159 <your_email@example.com>
2025-04-22 00:54:58 +0600

    Chauffeur Service User Switch Scence

--- src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerMaintainanceHistoryController.java
+++ src/main/java/com/example/cse213finalproject/sahkib/controller/CarOwnerMaintainanceHistoryController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class CarOwnerMaintainanceHistoryController {
--- src/main/java/com/example/cse213finalproject/sahkib/controller/ChauffeurServiceUserReviewController.java
+++ src/main/java/com/example/cse213finalproject/sahkib/controller/ChauffeurServiceUserReviewController.java
@@ -1,6 +1,16 @@
 package com.example.cse213finalproject.sahkib.controller;
 
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.example.cse213finalproject.commonClass.SceneSwitch.switchScene;
 
 public class ChauffeurServiceUserReviewController {
     @javafx.fxml.FXML
@@ -10,5 +20,18 @@ public class ChauffeurServiceUserReviewController {
 
     @javafx.fxml.FXML
     public void backAction(ActionEvent actionEvent) {
+        switchScene("/com/example/cse213finalproject/sahkib/ChauffeurServiceUserDashBoardView.fxml", event);
+    }
+    private void switchScene(String fxmlFile, Event event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
